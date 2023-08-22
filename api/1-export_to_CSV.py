0da9e7a1cdbb7bf6d1fd11d37b389871f19ec655
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-08-21 21:50:16 -0500

    update the file for one fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 """Gathering the needed informations from the API."""
 import csv
-import json
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    resp_users = requests.get('https://jsonplaceholder.typicode.com/users')
-    resp_todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    url = 'https://jsonplaceholder.typicode.com'
+    user_id = argv[1]
+    response = requests.get(
+        f'{url}/users/{user_id}/todos',
+        params={'_expand': 'user'}
+    )
 
-    for i in resp_users.json():
-        if i['id'] == int(argv[1]):
-            emp = i['username']
-    with open(f'{argv[1]}.csv', 'w') as f:
-        for i in resp_todos.json():
-            if i['userId'] == int(argv[1]):
-                c = i['completed']
-                t = i['title']
-                f.write(f"\"{argv[1]}\",\"{emp}\",\"{c}\",\"{t}\"\n")
+    data = response.json()
+    name = data[0]['user']['name']
+    fileName = f"{user_id}.csv"
+
+    with open(fileName, "w", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
+        for task in data:
+            writer.writerow(
+                [user_id, name, str(task["completed"]),
+                 task["title"]]
+            )
+
