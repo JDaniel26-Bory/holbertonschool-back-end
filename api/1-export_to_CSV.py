00a598e5f150b1c49103e3113c15634c28788512
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-08-21 21:43:30 -0500

    update the file for one fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Import Modules"""
+"""Gathering the needed informations from the API."""
 import csv
+import json
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    url_users = requests.get('https://jsonplaceholder.typicode.com/users')
-    url_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    resp_users = requests.get('https://jsonplaceholder.typicode.com/users')
+    resp_todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
 
-    for i in url_users.json():
+    for i in resp_users.json():
         if i['id'] == int(argv[1]):
-            username = i['username']
-    with open(f"{argv[1]}.csv", 'w') as f:
-        for i in url_todos.json():
+            emp = i['username']
+    with open(f'{argv[1]}.csv', 'w') as f:
+        for i in resp_todos.json():
             if i['userId'] == int(argv[1]):
-                completed = i['completed']
-                title = i['title']
-                f.write("\"{}\",\"{}\",\"{}\",\"{}\"\n".
-                        format(argv[1], username, completed, title))
+                c = i['completed']
+                t = i['title']
+                f.write(f"\"{argv[1]}\",\"{emp}\",\"{c}\",\"{t}\"\n")
