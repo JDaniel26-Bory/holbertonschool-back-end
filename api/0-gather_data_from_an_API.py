6af1bcf28858ecebd3810e795b19be89fc6cad60
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-08-21 20:08:34 -0500

    update the files

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-"""Import Modules"""
+'''returns information about his/her TODO list progress.'''
 import requests
-import sys
 from sys import argv
 
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
+    user_id = argv[1]
+    response = requests.get(
+        f'{url}/users/{user_id}/todos',
+        params={'_expand': 'user'}
+    )
 
-if __name__ == "__main__":
+    if response.status_code == 200:
+        data = response.json()
+        name = data[0]['user']['name']
+        tasks_ok = [task for task in data if task['completed']]
+        n_task_ok = len(tasks_ok)
+        total_task = len(data)
 
-    EMPLOYEE_ID = argv[1]
-    response_users = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(EMPLOYEE_ID))
-    response_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'
-        .format(EMPLOYEE_ID))
+        first_str = f"Employee {name} is done with tasks"
 
-    if response_users.status_code != 200 or response_todos.status_code != 200:
-        print("Error: {}".format(
-            response_users.status_code or response_todos.status_code))
-        sys.exit(1)
+        print(f"{first_str}({n_task_ok}/{total_task}):")
+        for task in tasks_ok:
+            print(f"\t {task['title']}")
 
-    user_data = response_users.json()
-    todos_data = response_todos.json()
-
-    employee_name = user_data.get('name')
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
-
-    print("Employee {} is done with tasks({}/{})".format(
-        employee_name, completed_tasks, total_tasks))
-
-    for todo in todos_data:
-        if todo['completed']:
-            print(f"\t{todo['title']}")
+    else:
+        print(f"Error: {response.status_code}")
